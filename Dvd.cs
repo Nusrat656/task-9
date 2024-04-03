c60b3119e318127e99852d14fd8497e6d7ef5d96
Nusrat656 <161738911+Nusrat656@users.noreply.github.com>
2024-04-03 16:49:56 +0400

    Add files via upload

---
+++ Dvd.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp15.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public class Dvd : ILibraries
+    {
+        public Dvd(string v1, string v2, int v3)
+        {
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int AvailabeCopies { get; set; }
+
+
+        public void CheckOut()
+        {
+            if (AvailabeCopies > 0)
+            {
+                AvailabeCopies--;
+                Console.WriteLine("Available");
+            }
+            else { Console.WriteLine("Not Availabe"); }
+
+        }
+
+        public void Return()
+        {
+
+            AvailabeCopies++;
+            Console.WriteLine("Kopya artdi");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleApp18;
+
+using ConsoleApp18.Daire;
+
+Shape circle = new Circle(5);
+Shape square = new Square(4);
+Shape triangle = new Triangle(3, 6);
+Console.WriteLine($"Circle Area: {circle.CalculateArea()}");
+Console.WriteLine($"Square Area: {square.CalculateArea()}");
+Console.WriteLine($"Triangle Area: {triangle.CalculateArea()}");
